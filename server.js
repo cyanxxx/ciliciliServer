6f69813b09addd073474b82241dc00d469f9cd0e
Your Name <you@example.com>
2018-06-06 16:26:17 +0800

    fix page
    Former-commit-id: 9ab195c1ce35ece26d154bdfff109b445af7a418

--- server.js
+++ server.js
@@ -4,27 +4,15 @@ const app = express();
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
-// 跨域设置
-app.all('*', function (req, res, next) {
-  res.header("Access-Control-Allow-Credentials", true)
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
-  res.header("X-Powered-By", ' 3.2.1')
-  res.header("Content-Type", "application/x-www-form-urlencoded")
-  next()
-})
-
 app.use(require('connect-history-api-fallback')())
-//app.use(express.static('public'));
+app.use(express.static('public'));
 app.use('/oauth2/access_token', require('./router/auth'))
 app.use('/users/show.json', require('./router/userInfo'))
 app.use('/2/statuses/home_timeline.json', require('./router/timeline'))
 
 
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 app.listen(port, () => {
     console.log(`server running @${port}`);
 })
